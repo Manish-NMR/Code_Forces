39a6823d5aa2a6bfa6dc10f1c9978f9e837633ce
Manish-NMR <2016nmr2016@gmail.com>
2023-09-27 23:23:29 +0530

    32B - Borze

--- B_Borze.cpp
+++ B_Borze.cpp
@@ -1,10 +1,10 @@
+// link - https://codeforces.com/problemset/problem/32/B
+
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main(){
-    // if . check for previos 0 -> . , 1 -> -. , 2 -> --
-    // if - check for next
     string s;
     cin>>s;
     for(int i=0;i<s.length();i++){
@@ -20,3 +20,16 @@ int main(){
             }
     }
 }
+
+// inputCopy
+// .-.--
+// outputCopy
+// 012
+// inputCopy
+// --.
+// outputCopy
+// 20
+// inputCopy
+// -..-.--
+// outputCopy
+// 1012
