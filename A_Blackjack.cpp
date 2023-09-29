c7f962e877c38d3a51a27b23b7d6bf0f4fc0cb9c
Manish-NMR <2016nmr2016@gmail.com>
2023-09-29 20:50:08 +0530

    104A - Blackjack

---
+++ A_Blackjack.cpp
@@ -0,0 +1,16 @@
+// link - 
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if (n <= 10 || n > 21)
+            cout << 0 << endl;
+        else if (n == 20)
+            cout << 15 << endl;
+        else
+            cout << 4 << endl;
+        return 0;
+}
