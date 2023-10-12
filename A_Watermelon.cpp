6f5adc3a511d895a0212a42dcf6e4422c29e8239
Manish-NMR <2016nmr2016@gmail.com>
2023-10-12 22:09:02 +0530

    4A - Watermelon

---
+++ A_Watermelon.cpp
@@ -0,0 +1,19 @@
+// link - https://codeforces.com/problemset/problem/4/A
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if(n == 2) cout<<"NO"<<endl;
+    else if(n%2==0) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+
+    return 0;
+}
+
+// inputCopy
+// 8
+// outputCopy
+// YES
